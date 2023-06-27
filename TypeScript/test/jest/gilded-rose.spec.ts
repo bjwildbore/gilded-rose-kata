e6d027a068f8715f188edea41a9931b85e58c95f
bjwildbore <bjwildbore@gmail.com>
2023-06-27 22:24:46 +1200

    First jest test

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -1,9 +1,13 @@
-import { Item, GildedRose } from '@/gilded-rose';
+import { Item, GildedRose } from '@/gilded-rose'
 
-describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
-  });
-});
+describe('normal item tests', () => {
+  test('should decrease in quality and sellIn', () => {
+    let items = [new Item('generic item', 3, 3)]
+    const gildedRose = new GildedRose(items)
+    gildedRose.updateQuality()
+
+    expect(items[0].name).toBe('generic item')
+    expect(items[0].sellIn).toBe(2)
+    expect(items[0].quality).toBe(2)
+  })
+})
